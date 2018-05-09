06f78cbf1e3af5f128bf070b46ab67542926650f
Zhiquan Li <lzq100123@qq.com>
2018-05-08 22:57:58 -0500

    added SLF4J Logging facility successfully

--- onlinemarketingsystem/src/main/java/net/lzq/onlinemarketingsystem/controller/PageController.java
+++ onlinemarketingsystem/src/main/java/net/lzq/onlinemarketingsystem/controller/PageController.java
@@ -1,5 +1,7 @@
 package net.lzq.onlinemarketingsystem.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import net.lzq.marketingbackend.dto.Product;
 @Controller
 public class PageController {
 		
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+	
 	@Autowired
 	private CategoryDAO categoryDAO;
 	@Autowired
@@ -21,6 +25,9 @@ public class PageController {
 	
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index(){
+		
+		logger.info("Inside PageController index method-INFO");
+		logger.debug("Inside PageController index method-DEBUG");
 		
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
